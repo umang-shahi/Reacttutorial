7f0e393ff5b15fdff73b9f8f9e00686e1c16bc4a
umang <umangshahi800@gmail.com>
2022-11-28 16:43:44 +0545

    add expression

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import umang from "../image/umang.jpg" ;
+import "./home.css";
+   
+
 
 const Home = () => {
+  
   return (
-      <div>Home</div>
+  
+
+
+    <>
+     <h1  className="heading">Wakanda forever</h1>
+    <diV className ="img_div">
+
+
+      <img src={umang} alt= "umang" />
+      <img src={umang} alt= "umang" />
+      <img src={umang} alt= "umang" />
+
+    </diV>
+
+   
+    </>
+
+
+      
   )
 }
 
 export default Home;
+
+
 
